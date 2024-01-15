b0c4a163a23faf58af204b0b213e69722d297257
Andrey Kirichenko <rclongloaf@ya.ru>
2024-01-15 20:40:54 +0300

    add base for voxel editor

---
+++ Assets/Main/Scripts/Helpers/CursorUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Main.Scripts.Helpers
+{
+public static class CursorUtils
+{
+    public static void GetCursorWorldPosition(Camera camera, Plane plane, out Vector3 cursorPosition)
+    {
+        var ray = camera.ScreenPointToRay(Input.mousePosition);
+        cursorPosition = plane.Raycast(ray, out var cursorDistance) ? ray.GetPoint(cursorDistance) : Vector3.zero;
+    }
+
+    public static Vector3 GetCursorOffsetNormalized()
+    {
+        var mousePosition = Input.mousePosition;
+        var minScreenSize = Math.Min(Screen.width, Screen.height);
+        return new Vector3(
+            (mousePosition.x - Screen.width / 2f) / minScreenSize * 2,
+            0,
+            (mousePosition.y - Screen.height / 2f) / minScreenSize * 2
+        );
+    }
+}
+}
